54a0c0ce38ea50f18d63da7acb868937c7187554
MaxVidom <MaxVidomenko@gmail.com>
2022-10-24 18:28:00 +0300

    Complete all tasks

--- controllers/contacts/add.js
+++ controllers/contacts/add.js
@@ -5,9 +5,10 @@ const add = async (req, res, next) => {
     const {error} = schemas.addSchema.validate(req.body)
     if (error) {
       throw RequestError(400, 'missing required name field"');
-    }
-  const contact = req.body; 
-    const result = await Contact.create(contact);
+  }
+  const contact = req.body;
+  const { id: owner } = req.user;
+    const result = await Contact.create({...contact, owner});
     res.status(201).json(result)
 }
   
